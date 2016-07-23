67a04a6c4d6c601b38007961c2259143b1babd99
Travis <noreply@travis-ci.org>
2016-07-23 01:10:30 +0000

    Deploy to GitHub Pages

---
+++ implementors/core/ops/trait.AddAssign.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["libc"] = [];implementors["geo"] = ["impl&lt;T&gt; <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.AddAssign.html' title='core::ops::AddAssign'>AddAssign</a> for <a class='struct' href='geo/struct.Bbox.html' title='geo::Bbox'>Bbox</a>&lt;T&gt; <span class='where'>where T: <a class='trait' href='num_traits/float/trait.Float.html' title='num_traits::float::Float'>Float</a> + <a class='trait' href='num_traits/cast/trait.ToPrimitive.html' title='num_traits::cast::ToPrimitive'>ToPrimitive</a></span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
