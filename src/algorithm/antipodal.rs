6faf4f80c9f34715d37148979dc571b707aa00ea
Stephan Hügel <stephan.hugel.12@ucl.ac.uk>
2017-03-21 15:32:33 +0000

    WIP: fixed index issues

--- src/algorithm/antipodal.rs
+++ src/algorithm/antipodal.rs
@@ -87,18 +87,19 @@ fn antipodal<T>(mut points: &mut [Point<T>]) -> Vec<(Point<T>, Point<T>)>
     let upper = points[..last_upper].to_vec();
     let last_lower = partition(&mut points, |p| point_location(min, max, p));
     let lower = points[..last_lower].to_vec();
-    // Now we have U and L, apply rotating callipers
     let mut antipodal: Vec<(Point<T>, Point<T>)> = vec![];
     let mut i = 0;
     let mut j = lower.len() - 1;
     while i < upper.len() - 1 || j > 0 {
         antipodal.push((upper[i], lower[j]));
-        // if i or j made it all the way through
-        // advance other size
         if i == upper.len() - 1 {
+            // we've walked all the way along the upper hull
             j = j - 1;
         } else if j == 0 {
+            // we've walked all the way along the lower hull
             i = i + 1;
+            // There are points remaining, so compare the slopes of next hull edges
+            // Could we have divide-by-0 errors here?
         } else if (upper[i + 1].y() - upper[i].y()) * (lower[j].x() - lower[j - 1].x()) >
                   (upper[i + 1].x() - upper[i].x()) * (lower[j].y() - lower[j - 1].y()) {
             i = i + 1;
@@ -106,7 +107,6 @@ fn antipodal<T>(mut points: &mut [Point<T>]) -> Vec<(Point<T>, Point<T>)>
             j = j - 1;
         }
     }
-    println!("{:?}", antipodal);
     antipodal
 }
 
@@ -119,7 +119,13 @@ mod test {
         let points_raw = vec![(5., 1.), (4., 2.), (4., 3.), (5., 4.), (6., 4.), (7., 3.),
                               (7., 2.), (6., 1.), (5., 1.)];
         let mut points = points_raw.iter().map(|e| Point::new(e.0, e.1)).collect::<Vec<_>>();
+        // currently have no idea whether this result is correct
         let antipodes = antipodal(points.as_mut_slice());
-        assert_eq!(antipodes, vec![(Point::new(0.0, 1.0), Point::new(2.0, 3.0))]);
+        let correct = vec![(Point::new(5.0, 1.0), Point::new(6.0, 4.0)),
+                           (Point::new(5.0, 1.0), Point::new(5.0, 4.0)),
+                           (Point::new(5.0, 1.0), Point::new(4.0, 3.0)),
+                           (Point::new(6.0, 1.0), Point::new(4.0, 3.0)),
+                           (Point::new(5.0, 1.0), Point::new(4.0, 3.0))];
+        assert_eq!(antipodes, correct);
     }
 }
